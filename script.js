e2f9dd67d17ef2197389cc664a1d47ca7f21320b
Ikuerowodamilare <ikuerowoabimbola@gmail.com>
2024-05-07 20:22:46 -0400

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector(".container");
 const optionsContainer = document.querySelector(".options-container");
-const country = "in";
+const country = "ca";
 const options = ["General",
 "Entertainment",
 "Health",
@@ -54,6 +54,9 @@ const init = () => {
 };
 
 window.onload = () => {
-    requestURL = 'https://newsapi.org/v2/top-headlines?country=${country}&category=general&apiKey=${apiKey}';
+    const country = 'ca'; // Replace 'your_country_code' with the desired country code
+    const apiKey = 'c595de6b0242465dbca75892acea08cb'; // Replace 'your_api_key' with your actual API key
+
+    const requestURL = `https://newsapi.org/v2/top-headlines?country=${country}&category=general&apiKey=${apiKey}`;
     init(); 
 };
